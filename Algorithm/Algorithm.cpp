476d010c6d681e3c3f8d556266ad68e803768b0d
minpark96 <hmpark3355@gmail.com>
2024-01-08 17:53:22 +0900

    연결 리스트

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,101 +1,103 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
 template<typename T>
-class Vector
+class Node
 {
 public:
-	Vector()
+	Node() : _prev(nullptr), _next(nullptr), _data(T())
 	{
 
 	}
 
-	~Vector()
+	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
 	{
-		if (_data)
-			delete[] _data;
+
+	}
+
+private:
+	Node*	_prev;
+	Node*	_next;
+	T		_data;
+};
+
+template<typename T>
+class List
+{
+public:
+	List() : _size(0)
+	{
+		_head = new Node<T>();
+		_tail = new Node<T>();
+		_head->_next = _tail;
+		_tail->_prev = _head;
+	}
+
+	~List()
+	{
+		delete _head;
+		delete _tail;
 	}
 
 	void push_back(const T& value)
 	{
-		if (_size == _capacity)
-		{
-			// 증설 작업
-			int newCapacity = static_cast<int>(_capacity * 1.5);
-			if (newCapacity == _capacity)
-				newCapacity++;
-
-			reserve(newCapacity);
-		}
-
-		// 데이터 저장
-		_data[_size] = value;
-
-		// 데이터 개수 증가
-		_size++;
-	}
-
-	void reserve(int capacity)
-	{
-		if (_capacity >= capacity)
-			return;
-
-		_capacity = capacity;
-
-		T* newData = new T[_capacity];
-
-		// 데이터 복사
-		for (int i = 0; i < _size; i++)
-			newData[i] = _data[i];
-
-		if (_data)
-			delete[] _data;
-
-		// 교체
-		_data = newData;
 
 	}
 
-	T& operator[](const int pos) { return _data[pos]; }
-
-	int size() { return _size; }
-	int capacity() { return _capacity; }
-
-	void clear()
+	void pop_back()
 	{
-		if (_data)
-		{
-			delete[] _data;
-			_data = new T[_capacity];
-		}
 
-		_size = 0;
 	}
 
 private:
-	T*		_data = nullptr;
-	int		_size = 0;
-	int		_capacity = 0;
+	Node<T>* AddNode(Node<T>* before, const T& value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* prevNode = before->_prev;
+
+		prevNode->_next = newNode;
+		newNode->_prev = prevNode;
+
+		newNode->_next = before;
+		before->_prev = newNode;
+	}
+
+public:
+
+
+
+
+private:
+	Node<T>*	_head;
+	Node<T>*	_tail;
+	int			_size;
 };
 
 int main()
 {
-	vector<int> v;
+	list<int> li;
 
-	v.reserve(100);
-	cout << v.size() << " " << v.capacity() << endl;
-
-	for (int i = 0; i < 100; i++)
+	list<int>::iterator eraseIt;
+	for (int i = 0; i < 10; i++)
 	{
-		v.push_back(i);
-		cout << v.size() << " " << v.capacity() << endl;
-	}	
-	
-	v.resize(10);
-	cout << v.size() << " " << v.capacity() << endl;
+		if (i == 5)
+		{
+			eraseIt = li.insert(li.end(), i);
+		}
+		else
+		{
+			li.push_back(i);
+		}
+	}
 
-	v.clear();
-	cout << v.size() << " " << v.capacity() << endl;
+	li.pop_back();
 
+	li.erase(eraseIt);
+
+	for (list<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << (*it) << endl;
+	}
 }
