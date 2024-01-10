b701079513b9557b195428534ab84de42c2062ab
minpark96 <hmpark3355@gmail.com>
2024-01-10 15:00:24 +0900

    연결리스트

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -17,10 +17,74 @@ public:
 
 	}
 
-private:
+public:
 	Node*	_prev;
 	Node*	_next;
 	T		_data;
+};
+
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _node(nullptr)
+	{
+
+	}
+
+	Iterator(Node<T>* node) : _node(node)
+	{
+
+	}
+
+	// ++it
+	Iterator& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+
+	// it++
+	Iterator operator++(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_next;
+		return temp;
+	}
+
+	// --it
+	Iterator& operator--()
+	{
+		_node = _node->_prev;
+		return *this;
+	}
+
+	// it--
+	Iterator operator--(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_prev;
+		return temp;
+	}
+
+	// *it
+	T& operator*()
+	{
+		return _node->_data;
+	}
+
+	bool operator==(const Iterator& other)
+	{
+		return _node == other._node;
+	}
+
+	bool operator!=(const Iterator& other)
+	{
+		return _node != other._node;
+	}
+
+public:
+	Node<T>* _node;
 };
 
 template<typename T>
@@ -29,6 +93,7 @@ class List
 public:
 	List() : _size(0)
 	{
+		// [head] <-> ... <-> [tail]
 		_head = new Node<T>();
 		_tail = new Node<T>();
 		_head->_next = _tail;
@@ -37,21 +102,26 @@ public:
 
 	~List()
 	{
+		while (_size > 0)
+			pop_back();
 		delete _head;
 		delete _tail;
 	}
 
 	void push_back(const T& value)
 	{
-
+		AddNode(_tail, value);
 	}
 
 	void pop_back()
 	{
-
+		RemoveNode(_tail->_prev);
 	}
 
 private:
+
+	// [head] <-> [1] <-> [prevNode] <-> [before] <-> [tail]
+	// [head] <-> [1] <-> [prevNode] <-> [newNode] <-> [before] <-> [tail]
 	Node<T>* AddNode(Node<T>* before, const T& value)
 	{
 		Node<T>* newNode = new Node<T>(value);
@@ -62,12 +132,48 @@ private:
 
 		newNode->_next = before;
 		before->_prev = newNode;
+
+		_size++;
+
+		return newNode;
 	}
 
+	// [head] <-> [prevNode] <-> [node] <-> [nextNode] <-> [tail]
+	// [head] <-> [prevNode] <-> [nextNode] <-> [tail]
+	Node<T>* RemoveNode(Node<T>* node)
+	{
+		Node<T>* prevNode = node->_prev;
+		Node<T>* nextNode = node->_next; 
+
+		prevNode->_next = nextNode;
+		nextNode->_prev = prevNode;
+
+		delete node;
+
+		_size--;
+
+		return nextNode;
+	}
+
+	int size() { return _size; }
+
 public:
+	using iterator = Iterator<T>;
+	iterator begin() { return iterator(_head->_next); }
+	iterator end() { return iterator(_tail); }
 
+	// it '앞에' 추가
+	iterator insert(iterator it, const T& value)
+	{
+		Node<T>* node = AddNode(it._node, value);
+		return iterator(node);
+	}
 
-
+	iterator erase(iterator it)
+	{
+		Node<T>* node = RemoveNode(it._node);
+		return iterator(node);
+	}
 
 private:
 	Node<T>*	_head;
@@ -77,9 +183,9 @@ private:
 
 int main()
 {
-	list<int> li;
+	List<int> li;
 
-	list<int>::iterator eraseIt;
+	List<int>::iterator eraseIt;
 	for (int i = 0; i < 10; i++)
 	{
 		if (i == 5)
@@ -96,7 +202,7 @@ int main()
 
 	li.erase(eraseIt);
 
-	for (list<int>::iterator it = li.begin(); it != li.end(); it++)
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
 	{
 		cout << (*it) << endl;
 	}
