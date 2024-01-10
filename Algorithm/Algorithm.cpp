21b4e4b654c6dfe4e1b99756813557892f71a359
minpark96 <hmpark3355@gmail.com>
2024-01-10 17:47:50 +0900

    큐

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -2,69 +2,86 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <queue>
 using namespace std;
 
-// Stack (LIFO Last-In-First-Out 후입선출)
+// Quque (FIFO First-In-First-Out 선입선출)
 
-// [1][2][3][4] << 밀어넣음
-// 되돌리기 (Ctrl+Z)
+// front << [1][2][3][4] << rear(back)
+// 대기열
 
-template<typename T, typename Container = vector<T>>
-class Stack
+template<typename T>
+class ArrayQueue
 {
 public:
+	ArrayQueue()
+	{
+		_container.resize(100);
+	}
+
 
 	void push(const T& value)
 	{
-		_container.push_back(value);
-	}
+		// TODO : 다 찼는지 체크
+		if (_size == _container.size())
+		{
+			// 증설 작업
+			int newSize = max(1, _size * 2); // 처음에 0이니깐
+			vector<T> newData;
+			newData.resize(newSize);
+			
+			// 데이터 복사
+			for (int i = 0; i < _size; i++)
+			{
+				int index = (_front + i) % _container.size();
+				newData[i] = _container[index];
+			}
 
-	T pop()
-	{
-		T ret = _data[_size - 1];
-		_size--;
-		return ret;
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size(); // back을 다시 유효 범위로 설정
+		_size++; // 데이터의 개수
 	}
 
 	void pop()
 	{
-		_container.pop_back();
+		_front = (_front + 1) % _container.size();
+		_size--;
 	}
 
-	T& top()
+	T& front()
 	{
-		return _container.back();
+		return _container[_front];
 	}
 
-	bool empty() { return _container.empty(); }
-	int size() { return _container.size(); }
+	bool empty() { return _size == 0; }
+	int size() { return _size; }
 
 private:
-	// 컨테이너를 바꿔도 가능 STL 인터페이스 통일의 장점
-	//vector<T> _container;
-	//list<T> _container;
-	Container _container;
+	vector<T> _container; // 순환 구조 original은 deque
+
+	int _front = 0;
+	int _back = 0;
+	int _size = 0;
 };
 
 int main()
 {
-	Stack<int> s;
+	ArrayQueue<int> q;
 
-	// 삽입
-	s.push(1);
-	s.push(2);
-	s.push(3);
+	for (int i = 0; i < 100; i++)
+		q.push(i);
 
-	// 비었나?
-	while (s.empty() == false)
+	while (q.empty() == false)
 	{
-		// 최상위 원소
-		int data = s.top();
-		// 최상위 원소 삭제
-		s.pop();
-
-		cout << data << endl;
+		int value = q.front();
+		q.pop();
+		cout << value << endl;
 	}
 
-	int size = s.size();
-}
+	int size = q.size();
+}	
