4a71d1eb4aaf3ecd45e77832aaecb3546831f9a5
minpark96 <hmpark3355@gmail.com>
2024-01-15 17:44:29 +0900

    오른손 법칙 스택 적용, 그래프 기초

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -5,83 +5,122 @@
 #include <queue>
 using namespace std;
 
-// Quque (FIFO First-In-First-Out 선입선출)
-
-// front << [1][2][3][4] << rear(back)
-// 대기열
-
-template<typename T>
-class ArrayQueue
+void CreateGraph_1()
 {
-public:
-	ArrayQueue()
+	struct Vertex
 	{
-		_container.resize(100);
-	}
+		vector<Vertex*> edges;
+		// int data;
+	};
 
+	vector<Vertex> v;
+	v.resize(6);
 
-	void push(const T& value)
+	v[0].edges.push_back(&v[1]);
+	v[0].edges.push_back(&v[3]);
+	v[1].edges.push_back(&v[0]);
+	v[1].edges.push_back(&v[2]);
+	v[1].edges.push_back(&v[3]);
+	v[3].edges.push_back(&v[4]);
+	v[5].edges.push_back(&v[4]);
+
+	bool connected = false;
+	for (Vertex* edge : v[0].edges)
 	{
-		// TODO : 다 찼는지 체크
-		if (_size == _container.size())
+		if (edge == &v[3])
 		{
-			// 증설 작업
-			int newSize = max(1, _size * 2); // 처음에 0이니깐
-			vector<T> newData;
-			newData.resize(newSize);
-			
-			// 데이터 복사
-			for (int i = 0; i < _size; i++)
-			{
-				int index = (_front + i) % _container.size();
-				newData[i] = _container[index];
-			}
-
-			_container.swap(newData);
-			_front = 0;
-			_back = _size;
+			connected = true;
+			break;
 		}
-
-		_container[_back] = value;
-		_back = (_back + 1) % _container.size(); // back을 다시 유효 범위로 설정
-		_size++; // 데이터의 개수
 	}
+}
 
-	void pop()
+void CreateGraph_2()
+{
+	struct Vertex
 	{
-		_front = (_front + 1) % _container.size();
-		_size--;
-	}
+		// int data;
+	};
 
-	T& front()
+	vector<Vertex> v;
+	v.resize(6);
+
+	// 연결된 목록을 따로 관리
+	// adjacent[n] -> n번째 정점과 연결된 정점 목록
+	vector<vector<int>> adjacent(6);
+	adjacent[0] = { 1, 3 };
+	adjacent[1] = { 0, 2, 3 };
+	adjacent[3] = { 4 };
+	adjacent[5] = { 4 };
+
+	// 정점이 100개
+	// - 지하철 노선도 -> 서로 드문 드문 연결 (양옆, 환승역이라면 조금 더 ++)
+	// - 페이스북 친구 -> 서로 빽빽하게 연결
+
+	bool connected = false;
+	for (int vertex : adjacent[0])
 	{
-		return _container[_front];
+		if (vertex == 3)
+		{
+			connected = true;
+			break;
+		}
 	}
 
-	bool empty() { return _size == 0; }
-	int size() { return _size; }
+	// STL
+	vector<int>& adj = adjacent[0];
+	bool connected2 = std::find(adj.begin(), adj.end(), 3) != adj.end();
+}
 
-private:
-	vector<T> _container; // 순환 구조 original은 deque
+void CreateGraph_3()
+{
+	struct Vertex
+	{
+		// int data;
+	};
 
-	int _front = 0;
-	int _back = 0;
-	int _size = 0;
-};
+	vector<Vertex> v;
+	v.resize(6);
+
+	// 연결된 목록을 따로 관리
+	// [X][O][X][O][X][X]
+	// [O][X][O][O][X][X]
+	// [X][X][X][X][X][X]
+	// [X][X][X][X][O][X]
+	// [X][X][X][X][X][X]
+	// [X][X][X][X][O][X]
+
+	// 읽는 방법 : adjacent[from][to]
+	// 행렬을 이용한 그래프 표현(2차원 배열)
+	// 메모리 소모가 심하지만, 빠른 접근이 가능하다.
+	// (간선이 많은 경우 이점이 있다)
+	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+	adjacent[0][1] = true;
+	adjacent[0][3] = true;
+	adjacent[1][0] = true;
+	adjacent[1][2] = true;
+	adjacent[1][3] = true;
+	adjacent[3][4] = true;
+	adjacent[5][4] = true;
+
+
+
+	bool connected = adjacent[0][3];
+
+	vector<vector<int>> adjacent2 =
+	{
+		vector<int> { -1, 15, -1, 35, -1, -1 },
+		vector<int> { 15, -1, +5, 10, -1, -1 },
+		vector<int> { -1, -1, -1, -1, -1, -1 },
+		vector<int> { -1, -1, -1, -1, +5, -1 },
+		vector<int> { -1, -1, -1, -1, -1, -1 },
+		vector<int> { -1, -1, -1, -1, +5, -1 },
+	}; 
+}
 
 int main()
 {
-	ArrayQueue<int> q;
-
-	for (int i = 0; i < 100; i++)
-		q.push(i);
-
-	while (q.empty() == false)
-	{
-		int value = q.front();
-		q.pop();
-		cout << value << endl;
-	}
-
-	int size = q.size();
+	CreateGraph_1();
+	CreateGraph_2();
+	CreateGraph_3();
 }	
