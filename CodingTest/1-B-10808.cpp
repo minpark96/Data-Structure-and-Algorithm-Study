4d519bb87aef6a17d9b49ff4cb8b5d53d4433daa
minpark96 <hmpark3355@gmail.com>
2024-01-28 19:34:12 +0900

    1-B-10808 / 최초 풀이

---
+++ CodingTest/1-B-10808.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#pragma region 최초풀이
+int main()
+{
+	string input;
+	int alpha[26] = { 0, };
+
+	cin >> input;
+
+	for (int i = 0; i < input.size(); i++)
+	{
+		char ch = input[i];
+
+		ch -= 'a';
+		alpha[ch]++;
+	}
+
+	for (int j = 0; j < 26; j++)
+	{
+		cout << alpha[j] << " ";
+	}
+
+	return 0;
+}
+#pragma endregion
