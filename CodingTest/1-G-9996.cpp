6aa79db9717625f81c259e41e18d2fc985416caa
minpark96 <hmpark3355@gmail.com>
2024-01-30 17:13:12 +0900

    1-G-9996 / 개선 2 - substr() 코드 개선

--- CodingTest/1-G-9996.cpp
+++ CodingTest/1-G-9996.cpp
@@ -102,15 +102,55 @@ using namespace std;
 
 #pragma endregion
 
-#pragma region 개선 2 - substr() 사용
+#pragma region 개선 1 - substr() 사용
+
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	string pat, str;
+//	cin >> pat;
+//	bool ret[101] = { false, };
+//	size_t pos = pat.find('*');
+//	string pre = pat.substr(0, pos);
+//	string suf = pat.substr(pos + 1);
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> str;
+//		if (str.size() < pre.size() + suf.size())
+//			continue;
+//		string temp = str.substr(0, pre.size());
+//		if (temp != pre) continue;
+//		temp.clear();
+//		temp = str.substr(str.size() - suf.size(), str.size());
+//		if (temp != suf) continue;
+//
+//		ret[i] = true;
+//	}
+//
+//	for (int j = 0; j < num; j++)
+//	{
+//		if (ret[j])
+//			cout << "DA" << "\n";
+//		else
+//			cout << "NE" << "\n";
+//	}
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 개선 2 - substr() 코드 개선
 
 int main()
 {
 	int num;
 	cin >> num;
 	string pat, str;
+
 	cin >> pat;
-	bool ret[101] = { false, };
 	size_t pos = pat.find('*');
 	string pre = pat.substr(0, pos);
 	string suf = pat.substr(pos + 1);
@@ -118,23 +158,15 @@ int main()
 	for (int i = 0; i < num; i++)
 	{
 		cin >> str;
-		if (str.size() < pre.size() + suf.size())
-			continue;
-		string temp = str.substr(0, pre.size());
-		if (temp != pre) continue;
-		temp.clear();
-		temp = str.substr(str.size() - suf.size(), str.size());
-		if (temp != suf) continue;
 
-		ret[i] = true;
-	}
-
-	for (int j = 0; j < num; j++)
-	{
-		if (ret[j])
-			cout << "DA" << "\n";
+		if (str.size() < pre.size() + suf.size()) cout << "NE" << "\n";
 		else
-			cout << "NE" << "\n";
+		{
+			if (pre == str.substr(0, pre.size()) && suf == str.substr(str.size() - suf.size()))
+				cout << "DA" << "\n";
+			else
+				cout << "NE" << "\n";
+		}
 	}
 
 	return 0;
