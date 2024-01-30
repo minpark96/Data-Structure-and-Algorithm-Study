517e416dca237a56030c6c53ac8bc8eebf2471b3
minpark96 <hmpark3355@gmail.com>
2024-01-30 17:12:14 +0900

    1-G-9996 / 개선 1 - substr() 사용

--- CodingTest/1-G-9996.cpp
+++ CodingTest/1-G-9996.cpp
@@ -52,39 +52,79 @@ using namespace std;
 
 #pragma region 최초 풀이 - 정답
 
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	string pat, str;
+//	bool ret[100] = { false, };
+//	cin >> pat;
+//
+//	string postfix, prefix;
+//	for (size_t j = 0; j < pat.size(); j++)
+//	{
+//		if (pat[j] != '*')
+//		{
+//			postfix += pat[j];
+//		}
+//		else
+//		{
+//			prefix = postfix;
+//			postfix.clear();
+//		}
+//	}
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> str;
+//		if (str.size() < prefix.size() + postfix.size())
+//			continue;
+//		size_t n = str.find(prefix, 0);
+//		if (n != 0)
+//			continue;
+//		n = str.find(postfix, str.size() - postfix.size());
+//		if (n != str.size() - postfix.size())
+//			continue;
+//
+//		ret[i] = true;
+//	}
+//
+//	for (int j = 0; j < num; j++)
+//	{
+//		if (ret[j])
+//			cout << "DA" << "\n";
+//		else
+//			cout << "NE" << "\n";
+//	}
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 개선 2 - substr() 사용
+
 int main()
 {
 	int num;
 	cin >> num;
 	string pat, str;
-	bool ret[100] = { false, };
 	cin >> pat;
-
-	string postfix, prefix;
-	for (size_t j = 0; j < pat.size(); j++)
-	{
-		if (pat[j] != '*')
-		{
-			postfix += pat[j];
-		}
-		else
-		{
-			prefix = postfix;
-			postfix.clear();
-		}
-	}
+	bool ret[101] = { false, };
+	size_t pos = pat.find('*');
+	string pre = pat.substr(0, pos);
+	string suf = pat.substr(pos + 1);
 
 	for (int i = 0; i < num; i++)
 	{
 		cin >> str;
-		if (str.size() < prefix.size() + postfix.size())
+		if (str.size() < pre.size() + suf.size())
 			continue;
-		size_t n = str.find(prefix, 0);
-		if (n != 0)
-			continue;
-		n = str.find(postfix, str.size() - postfix.size());
-		if (n != str.size() - postfix.size())
-			continue;
+		string temp = str.substr(0, pre.size());
+		if (temp != pre) continue;
+		temp.clear();
+		temp = str.substr(str.size() - suf.size(), str.size());
+		if (temp != suf) continue;
 
 		ret[i] = true;
 	}
