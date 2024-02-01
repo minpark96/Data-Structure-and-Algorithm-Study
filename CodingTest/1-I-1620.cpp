c6dea89e60abc4eb09e42550f62d25e2b80752c2
minpark96 <hmpark3355@gmail.com>
2024-02-01 16:24:10 +0900

    1-I-1620 / 개선 - atoi(), c_str() 사용

--- CodingTest/1-I-1620.cpp
+++ CodingTest/1-I-1620.cpp
@@ -6,8 +6,47 @@ using namespace std;
 
 #pragma region 최초 풀이
 
+//string names[100001];
+//string answers[100001];
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//	map <string, int> m_s;
+//	int n, m;
+//	cin >> n >> m;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		string temp;
+//		cin >> temp;
+//		names[i + 1] = temp;
+//		m_s[temp] = i + 1;
+//	}
+//
+//	for (int j = 0; j < m; j++)
+//	{
+//		cin >> answers[j];
+//		if (answers[j][0] < 'A')
+//		{
+//			int temp = stoi(answers[j]);
+//			cout << names[temp] << "\n";
+//		}
+//		else
+//		{
+//			cout << m_s[answers[j]] << "\n";
+//		}
+//	}
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 개선 - atoi(), c_str() 사용
+
+string str;
 string names[100001];
-string answers[100001];
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -18,23 +57,22 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		string temp;
-		cin >> temp;
-		names[i + 1] = temp;
-		m_s[temp] = i + 1;
+		cin >> str;
+		names[i + 1] = str;
+		m_s[str] = i + 1;
 	}
 
 	for (int j = 0; j < m; j++)
 	{
-		cin >> answers[j];
-		if (answers[j][0] < 'A')
+		cin >> str;
+		if (atoi(str.c_str()) == 0)
 		{
-			int temp = stoi(answers[j]);
-			cout << names[temp] << "\n";
+			cout << m_s[str] << "\n";
 		}
 		else
 		{
-			cout << m_s[answers[j]] << "\n";
+			int temp = atoi(str.c_str());
+			cout << names[temp] << "\n";
 		}
 	}
 
