a458a519c7e23b95a0784e6a8364896eb53bc215
minpark96 <hmpark3355@gmail.com>
2024-02-06 17:16:29 +0900

    1-L-1940 / 개선

--- CodingTest/1-L-1940.cpp
+++ CodingTest/1-L-1940.cpp
@@ -6,9 +6,45 @@ using namespace std;
 
 #pragma region 최초 풀이
 
+//int nums[15001];
+//int main()
+//{
+//	int n, m;
+//	int ret = 0;
+//
+//	cin >> n >> m;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> nums[i];
+//	}
+//
+//	sort(nums, nums + n);
+//	nums[n] = INT32_MAX;
+//
+//	for (int j = 0; nums[j] < m; j++)
+//	{
+//		for (int k = j + 1; k < n; k++)
+//		{
+//			if (nums[j] + nums[k] == m) ret++;
+//			else if (nums[j] + nums[k] > m) break;
+//		}
+//	}
+//
+//	cout << ret << "\n";
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 개선
+
 int nums[15001];
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
 	int n, m;
 	int ret = 0;
 
@@ -18,19 +54,23 @@ int main()
 		cin >> nums[i];
 	}
 
-	sort(nums, nums + n);
-	nums[n] = INT32_MAX;
-
-	for (int j = 0; nums[j] < m; j++)
+	if (m > 200000)
+		cout << 0 << "\n";
+	else
 	{
-		for (int k = j + 1; k < n; k++)
+		sort(nums, nums + n);
+		nums[n] = INT32_MAX;
+		for (int j = 0; nums[j] < m; j++)
 		{
-			if (nums[j] + nums[k] == m) ret++;
-			else if (nums[j] + nums[k] > m) break;
+			for (int k = j + 1; k < n; k++)
+			{
+				if (nums[j] + nums[k] == m) ret++;
+				else if (nums[j] + nums[k] > m) break;
+			}
 		}
-	}
 
-	cout << ret << "\n";
+		cout << ret << "\n";
+	}
 
 	return 0;
 }
