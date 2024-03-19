57ebe65a8b5618cf6b65d8672e5d436aacd540bb
minpark96 <hmpark3355@gmail.com>
2024-03-19 11:09:51 +0900

    2-F-2828 / 개선

--- CodingTest/2-F-2828.cpp
+++ CodingTest/2-F-2828.cpp
@@ -9,50 +9,85 @@ using namespace std;
 
 #pragma region 최초 풀이
 
-int n, m, j;
-int s[15], bl, br, ret, pos;
+//int n, m, j;
+//int s[15], bl, br, ret, pos;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	cin >> n >> m;
+//	cin >> j;
+//
+//	bl = 1;
+//	br = m;
+//	for (int i = 0; i < j; i++)
+//	{
+//		cin >> pos;
+//		for (int j = 1; j < 15; j++)
+//		{
+//			if (j >= bl && j <= br)
+//				s[j] = 1;
+//			else
+//				s[j] = 0;
+//		}
+//
+//		if (s[pos] == 1) continue;
+//		else
+//		{
+//			if (pos < bl)
+//			{
+//				ret += (bl - pos);
+//				br -= (bl - pos);
+//				bl = pos;
+//			}
+//			else
+//			{
+//				ret += (pos - br);
+//				bl += (pos - br);
+//				br = pos;
+//			}
+//		}
+//	}
+//
+//	cout << ret << "\n";
+//
+//	return 0;
+//}
 
+#pragma endregion
+
+#pragma region 개선
+
+int n, m, j;
+int bl, br, ret, pos;
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-
-	cin >> n >> m;
-	cin >> j;
-
+	cin >> n >> m >> j;
 	bl = 1;
-	br = m;
 	for (int i = 0; i < j; i++)
 	{
 		cin >> pos;
-		for (int j = 1; j < 15; j++)
-		{
-			if (j >= bl && j <= br)
-				s[j] = 1;
-			else
-				s[j] = 0;
-		}
-
-		if (s[pos] == 1) continue;
+		br = bl + m - 1;
+		if (pos >= bl && pos <= br) continue;
 		else
 		{
 			if (pos < bl)
 			{
 				ret += (bl - pos);
-				br -= (bl - pos);
 				bl = pos;
 			}
 			else
 			{
 				ret += (pos - br);
 				bl += (pos - br);
-				br = pos;
 			}
 		}
 	}
-
 	cout << ret << "\n";
-
 	return 0;
 }
 
