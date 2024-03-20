9b203f056c79ddf2c3da3de4e1dad8d2f0a5992e
minpark96 <hmpark3355@gmail.com>
2024-03-20 11:47:47 +0900

    2-H-4659 / 개선

--- CodingTest/2-H-4659.cpp
+++ CodingTest/2-H-4659.cpp
@@ -4,67 +4,128 @@ using namespace std;
 
 #pragma region 최초 풀이
 
-char vowel[] = { 'a', 'e', 'i', 'o', 'u' };
+//char vowel[] = { 'a', 'e', 'i', 'o', 'u' };
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	string end = "end";
+//
+//	while (true)
+//	{
+//		string str;
+//		bool hasV = false, isC = false, wasV = false, isAcc = true;
+//		char prev = NULL;
+//		cin >> str;
+//		if (str == end) break;
+//
+//		for (int i = 0; i < str.length(); i++)
+//		{
+//			bool isV = false;
+//			for (int j = 0; j < 5; j++)
+//			{
+//				if (str[i] == vowel[j])
+//				{
+//					hasV = true;
+//					isV = true;
+//					break;
+//				}
+//			}
+//			
+//			if (prev == str[i])
+//			{
+//				if (prev != 'e' && prev != 'o')
+//				{
+//					isAcc = false;
+//					break;
+//				}
+//			}
+//
+//			if (isV && wasV && isC)
+//			{
+//				isAcc = false;
+//				break;
+//			}
+//			else if (!isV && !wasV && isC)
+//			{
+//				isAcc = false;
+//				break;
+//			}
+//			else if (isV && wasV && i != 0)
+//				isC = true;
+//			else if (!isV && !wasV && i != 0)
+//				isC = true;
+//			else
+//				isC = false;
+//			
+//			wasV = isV;
+//			prev = str[i];
+//		}
+//
+//		if (isAcc && hasV)
+//		{
+//			cout << '<' << str << "> is acceptable." << "\n";
+//			continue;
+//		}
+//
+//		cout << '<' << str << "> is not acceptable." << "\n";
+//	}
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 개선
+
+bool isVowel(char idx)
+{
+	return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	string end = "end";
-
 	while (true)
 	{
 		string str;
-		bool hasV = false, isC = false, wasV = false, isAcc = true;
+		int cCnt = 0, vCnt = 0;
+		bool hasV = false, isAcce = true;
 		char prev = NULL;
 		cin >> str;
-		if (str == end) break;
+		if (str == "end") break;
 
-		for (int i = 0; i < str.length(); i++)
+		for (int i = 0; i < str.size(); i++)
 		{
-			bool isV = false;
-			for (int j = 0; j < 5; j++)
+			if (isVowel(str[i]))
 			{
-				if (str[i] == vowel[j])
-				{
-					hasV = true;
-					isV = true;
-					break;
-				}
+				cCnt = 0;
+				vCnt++;
+				hasV = true;
 			}
-			
-			if (prev == str[i])
-			{
-				if (prev != 'e' && prev != 'o')
-				{
-					isAcc = false;
-					break;
-				}
-			}
-
-			if (isV && wasV && isC)
-			{
-				isAcc = false;
-				break;
-			}
-			else if (!isV && !wasV && isC)
-			{
-				isAcc = false;
-				break;
-			}
-			else if (isV && wasV && i != 0)
-				isC = true;
-			else if (!isV && !wasV && i != 0)
-				isC = true;
 			else
-				isC = false;
-			
-			wasV = isV;
+			{
+				cCnt++;
+				vCnt = 0;
+			}
+			if (prev == str[i] && prev != 'e' && prev != 'o')
+			{
+				isAcce = false;
+				break;
+			}
+			if (cCnt == 3 || vCnt == 3)
+			{
+				isAcce = false;
+				break;
+			}
 			prev = str[i];
 		}
 
-		if (isAcc && hasV)
+		if (isAcce && hasV)
 		{
 			cout << '<' << str << "> is acceptable." << "\n";
 			continue;
